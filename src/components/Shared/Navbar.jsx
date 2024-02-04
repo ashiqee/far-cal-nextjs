3170e35ec44d8815cea12e1ccd1e5b21bb94924c
ashiqee <ashiq.buet73@gmail.com>
2024-02-04 08:27:47 +0600

    dashboard menu

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -21,6 +21,9 @@ const Navbar = () => {
          },
         {
         menuName:"About",path:'/about'
+         },
+        {
+        menuName:"Dashboard",path:'/dashboard'
          }
 ]
     return (
